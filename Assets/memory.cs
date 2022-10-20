b206cfd4a1bbeb6b05f780ccabe6405b5b4f16d9
DavidTran1311 <twitter13112@gmail.com>
2022-10-20 15:40:38 -0400

    Cards_Working_Editor

--- Assets/memory.cs
+++ Assets/memory.cs
@@ -1,69 +1,76 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Memory : MonoBehaviour
 {
-    string[] KCardSuits = new string[] { "Club", "Diamond", "Spades", "Heart" };
-    string[] KCardRanks = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    public static string[] Suits = new string[] { "Club", "Diamond", "Spades", "Heart" };
+    public static string[] Ranks = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    public Sprite[] Cards;
 
-    static public Memory instance;
+    public List<string> deck;
 
-    public Card[] cards;
-    public Card select;
-   
-
-    
     // Start is called before the first frame update
     void Start()
     {
+        PlayCards();
        
-        instance = this;
-
-       // cards = transform.GetComponentInChildren<cards>();
-        int n = 0;
-       
-       // Shuffle(Cards);
-         
-        for (int m=0;m<cards.Length/2;m++ )
-        {
-            string suit = GetRandomFromArray(KCardSuits);
-            string rank = GetRandomFromArray(KCardRanks);
-
-           // cards[n++].SetSuitAndRank(suit, rank);
-            //cards[n++].SetSuitAndRank(suit, rank);
-
-        }
-
-    }
-
-    private void Shuffle<T>(T[] array) { 
-
-        int n = array.Length;
-        while (n > 1) {
-
-            int k = (int)Mathf.Floor(Random.value * (n--));
-            T temp = array[n];
-            array[n] = array[k];
-            array[k] = temp;
-        }
-    
-    
-    }
-
-    private T GetRandomFromArray<T>(T[] array) {
-        return array[(int)Mathf.Floor(Random.value * array.Length)];
-    
     }
 
 
-   private void rankandsuit() { 
-    //set rank and suit
-    
-    }
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void PlayCards()
+    {
+
+        deck = GenenrateDeck();
+        Shuffle(deck);
+
+        foreach (string card in deck)
+        {
+            print(card);
+        }
+    }
+
+    public static List<string> GenenrateDeck()
+    {
+        List<string> newDeck = new List<string>();
+        foreach (string s in Suits)
+        {
+
+            foreach (string v in Ranks)
+            {
+
+                newDeck.Add(s + v);
+            }
+
+
+        }
+
+  
+
+        return newDeck;
+  }
+
+    void Shuffle<Temp>(List<Temp> list)
+    {
+
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+
+            int k = random.Next(n);
+            n--;
+            Temp temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+
+        }
+
     }
 }
